4443bd19d2d457c8abac9e4cdd9bfc526fcffc9a
Ocipa <ocipamail@gmail.com>
2021-09-20 11:42:07 -0600

    fix: will download fonts if config.fonts_dir is not a directory

--- sodgen/generate.py
+++ sodgen/generate.py
@@ -20,18 +20,20 @@ def generate(num: int=1, config=Config) -> str:
     ---
     -> return: a path to output_dir
     '''
-    assert config.output_dir, 'missing output_dir, change in config'
+    #assert config.output_dir, 'missing output_dir, change in config'
 
     images = []
     fonts = []
 
-    if not config.fonts_dir:
+    if not isinstance(config.fonts_dir, str):
         assert config.auto_download_fonts, 'missing fonts directory and auto download fonts is False in the config'
 
-        download_fonts_dir(path='./fonts_dir')
         fonts_dir = './fonts_dir'
     else:
         fonts_dir = config.fonts_dir
+    
+    if not os.path.isdir(fonts_dir):
+        download_fonts_dir(path=fonts_dir)
     
     assert os.path.isdir(fonts_dir), 'invalid fonts_dir'
 
