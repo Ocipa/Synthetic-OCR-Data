9d13698af1ec7a54ba34850af8ce7545a0c51d50
Ocipa <ocipamail@gmail.com>
2021-10-12 17:31:12 -0600

    refactor: rendering text now uses skia as the backend

--- sodgen/image.py
+++ sodgen/image.py
@@ -6,8 +6,11 @@ from sodgen.config import Config
 from sodgen.fonts import *
 from sodgen.corpus import *
 from sodgen.background import *
+from sodgen.utility import config_value_to_value as to_value
 
 from math import floor, ceil
+import matplotlib.pyplot as plt
+import skia # type: ignore
 
 import random
 import numpy as np
@@ -27,61 +30,33 @@ class image():
 
         self.texts = []
 
-        self._build()
-    
-    def _build(self):
-        width, height = self.config.size
-
-        #self.image = np.full((height, width, 3), fill_value=255, dtype=np.uint8)
         self.image = random_background(config=self.config)
-        
-        self.image = Image.fromarray(self.image, mode='RGBA')
 
+        self.surface = skia.Surface(self.image)
+        self.canvas = self.surface.getCanvas()
 
-        self.target_text_number = self.config.target_text_number
-        if isinstance(self.target_text_number, tuple):
-            self.target_text_number = random.randint(*self.target_text_number)
-        
+        self.target_text_number = to_value(self.config.target_text_number)
+
         for i in range(self.target_text_number):
             self._draw_text()
-        
-        self._combine_text()
-
 
     def _draw_text(self):
         font = random.choice(self.fonts)
 
         random_text = self.corpus.get_random_line(config=self.config)
         text = Text(font=font, text=random_text, config=self.config)
-        text.set_pos(texts=self.texts)
+        # TODO: find_pos needs to be called in image module
+        # since it needs the other texts in the image for when
+        # allow_overlap is false, maybe pass the other texts
+        # when building text class, and find_pos during __init__ ??
+        text.find_pos(texts=self.texts)
 
         if isinstance(text.pos, tuple):
+            # NOTE: if unable to find position for text, text.pos
+            # and text.bbox is None.
             self.texts.append(text)
 
-            #self._draw_mask(text)
-    
-    def _combine_text(self):
-        for i in self.texts:
-            text_image = np.full((self.config.size[1], self.config.size[0], 4), 0, dtype='uint8')
-            text_image = Image.fromarray(text_image, mode='RGBA')
-
-            pos = (int(i.pos[0] + i.render_offset[0]), int(i.pos[1] + i.render_offset[1]))
-            text_image.paste(Image.fromarray(i.render, mode='RGBA'), pos)
-
-            self.image = Image.alpha_composite(self.image, text_image)
-            
-            # self.image = Image.fromarray(self.image, 'RGBA')
-        
-        # for i in self.texts:
-        #     for i in i.lines_bbox:
-        #         self.image = utility.render_bounding_box(
-        #             self.image,
-        #             i
-        #         )
-        
-        self.image = self.image.convert('RGB')
-        #self.image = Image.fromarray(self.image)
-        # draw = ImageDraw.Draw(self.image)
+            text.render_text(self.canvas)
 
 
 
